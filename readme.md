cecc6b097a198a1dfa2720c8198136ec91589ecb
lidanqing-intel <danqing.li@intel.com>
2021-03-29 05:42:43 +0200

    update

--- readme.md
+++ readme.md
@@ -1,10 +1,4 @@
-Download paddle_inference lib, compile the application and run the application
+read the run.sh, change some params and run it
 ```
-wget https://paddle-inference-lib.bj.bcebos.com/2.0.0-cpu-avx-mkl/paddle_inference.tgz
-tar -xzvf paddle_inference.tgz
-mkdir build && cd build
-cmake .. -DPADDLE_LIB=$HOME/repo/Issue_28554/test_ocr_det/paddle_inference -DDEMO_NAME=model_test
-make -j 12
-cd ..
-./build/model_test --model_path=det_db --input_path=det_input.txt
+sh run.sh
 ```
--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
-#wget https://paddle-inference-lib.bj.bcebos.com/2.0.0-cpu-avx-mkl/paddle_inference.tgz
-#tar -xzvf paddle_inference.tgz
+# build paddle inference lib
+cd Paddle/build
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DWITH_GPU=OFF \
+      -DWITH_AVX=ON \
+      -DWITH_DISTRIBUTE=OFF \
+      -DWITH_MKLDNN=ON \
+      -DON_INFER=ON \
+      -DWITH_TESTING=ON \
+      -DWITH_INFERENCE_API_TEST=OFF \
+      -DWITH_NCCL=OFF \
+      -DWITH_PYTHON=ON \
+      -DPY_VERSION=3.6 \
+      -DWITH_LITE=OFF ..
+make -j 12
+make -j 12 inference_lib_dist
+
+# build application
 rm -rf build && mkdir build && cd build
-cmake .. -DPADDLE_LIB=/home/li/repo/Paddle/build/paddle_inference_install_dir -DDEMO_NAME=model_test
+cmake .. -DPADDLE_LIB=path/to/paddle_inference -DDEMO_NAME=model_test
 make -j 12
 cd ..
-./build/model_test --model_path=det_db --input_path=det_input.txt
+./build/model_test --model_path=det_db --input_path=det_input.txt > hardswish_fc_act_det_results.txt  2>&1
